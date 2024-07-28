906cb90a67850e2cd63e43234fc8f2b3e9be61d3
Sabuj Routh <sabujrouth2000@gmail.com>
2024-07-28 22:33:52 +0530

    style: Add 'active' class to navigation link when corresponding section is in view

--- script.js
+++ script.js
@@ -37,6 +37,9 @@ window.addEventListener('scroll', function () {
            if (scrollPos > section.offsetTop && scrollPos < (section.offsetTop + section.offsetHeight)) {
                 navLinks.forEach(link => {
                     link.classList.remove('active');
+                    if(section.getAttribute('id') === link.getAttribute('href').substring(1)) {
+                        link.classList.add('active')
+                    }
                     })
            }
         
