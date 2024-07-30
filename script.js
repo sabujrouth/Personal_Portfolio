da866e20f852c072589266610f3afe49eb680ef8
Sabuj Routh <sabujrouth2000@gmail.com>
2024-07-30 19:41:21 +0530

    style: Fix missing semicolon in script.js

--- script.js
+++ script.js
@@ -38,7 +38,7 @@ window.addEventListener('scroll', function () {
             navLinks.forEach(link => {
                 link.classList.remove('active');
                 if (section.getAttribute('id') === link.getAttribute('href').substring(1)) {
-                    link.classList.add('active')
+                    link.classList.add('active');
                 }
             });
         }
